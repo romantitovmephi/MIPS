d6128a9e56a9ac6ffa1d54b6ad05debbf57dd240
Roman Titov <107221825+romantitovmephi@users.noreply.github.com>
2022-08-29 12:56:20 +0000

    Update datapath.sv

--- src/datapath.sv
+++ src/datapath.sv
@@ -1,13 +1,13 @@
 module datapath (input  logic       clk, reset,
                  input  logic       memtoreg, pcsrc,
-					  input  logic       alusrc, regdst,
-					  input  logic       regwrite, jump,
-					  input  logic [2:0] alucontrol,
-					  output logic       zero,
-					  output logic [31:0] pc,
-					  input  logic [31:0] instr,
-					  output logic [31:0] aluout, writedata,
-					  input  logic [31:0] readdata);
+		 input  logic       alusrc, regdst,
+		 input  logic       regwrite, jump,
+		 input  logic [2:0] alucontrol,
+		 output logic       zero,
+		 output logic [31:0] pc,
+		 input  logic [31:0] instr,
+		 output logic [31:0] aluout, writedata,
+		 input  logic [31:0] readdata);
   logic [4:0] writereg;	
   logic [31:0] pcnext, pcnextbr, pcplus4, pcbranch;
   logic [31:0] signimm, signimmsh;  
